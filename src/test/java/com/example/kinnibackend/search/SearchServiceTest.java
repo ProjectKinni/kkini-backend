4081b471eb4aff156d9be3464acb2f84dca36042
Jayyyyp <pjae9726@gmail.com>
2023-09-06 17:47:26 +0900

    KN-134

--- src/test/java/com/example/kinnibackend/search/SearchServiceTest.java
+++ src/test/java/com/example/kinnibackend/search/SearchServiceTest.java
@@ -1,94 +1,145 @@
 package com.example.kinnibackend.search;
 
 import com.example.kinnibackend.dto.product.ProductCardListResponseDTO;
+import com.example.kinnibackend.entity.Product;
 import com.example.kinnibackend.exception.search.InvalidSearchTermException;
+import com.example.kinnibackend.exception.search.ProductNotFoundException;
+import com.example.kinnibackend.repository.product.ProductRepository;
 import com.example.kinnibackend.service.search.SearchService;
-import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.jdbc.Sql;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
-@TestInstance(TestInstance.Lifecycle.PER_METHOD)
-@Sql(scripts = "/data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 public class SearchServiceTest {
 
     @Autowired
     private SearchService searchService;
 
+    @Autowired
+    private ProductRepository productRepository;
+
+    private ProductCardListResponseDTO testProduct;
+
+    @BeforeEach
+    public void setUp() {
+        Product product = Product.builder()
+                .productName("Test Product")
+                .categoryName("Test Category")
+                .isGreen(true)
+                .detail("ddd")
+                .averageRating(4.52)
+                .makerName("슥")
+                .servingSize(100)
+                .kcal(200)
+                .carbohydrate(10)
+                .protein(20)
+                .fat(40)
+                .sodium(5)
+                .cholesterol(20)
+                .saturatedFat(10)
+                .transFat(5)
+                .sugar(2)
+                .score(54)
+                .image("~~~~")
+                .nutImage("~~~~~~~~")
+                .nutScore(20)
+                .build();
+
+        Product savedProduct = productRepository.save(product);
+
+        testProduct = ProductCardListResponseDTO.fromEntity(savedProduct);
+    }
+
+
     @Test
-    @Transactional
-    public void searchProductsByCategoryName() {
-        // given
-        String categoryName = "간식";
+    public void searchProducts_ValidSearchTerm_ShouldReturnProductList() {
+        // Given
+        String searchTerm = "Test";
 
-        // when
-        List<ProductCardListResponseDTO> results = searchService.searchProducts(categoryName);
+        // When
+        List<ProductCardListResponseDTO> result = searchService.searchProducts(searchTerm);
 
-        System.out.println(results);
-        // then
-        assertFalse(results.isEmpty());
-        assertTrue(results.stream().anyMatch(product -> "간식".equals(product.getCategoryName())));
+        // Then
+        assertThat(result).isNotEmpty();
+        assertThat(result.get(0).getProductName()).isEqualTo(testProduct.getProductName());
     }
 
     @Test
-    @Transactional
-    public void searchProductsByProductName() {
-        // given
-        String productName = "훈제";
+    public void searchProducts_InvalidSearchTerm_ShouldThrowException() {
+        // Given
+        String searchTerm = "";
 
-        // when
-        List<ProductCardListResponseDTO> results = searchService.searchProducts(productName);
-
-        // then
-        assertFalse(results.isEmpty());
-        assertTrue(results.stream().anyMatch(product -> product.getProductName().contains("훈제")));
+        // When & Then
+        assertThatThrownBy(() -> searchService.searchProducts(searchTerm))
+                .isInstanceOf(InvalidSearchTermException.class)
+                .hasMessageContaining("검색어가 유효하지 않습니다.");
     }
 
     @Test
-    @Transactional
-    public void getItemById() {
-        // given
-        Long productId = 1L;
+    public void searchProducts_ProductNotFound_ShouldThrowException() {
+        // Given
+        String searchTerm = "NonExisting";
 
-        // when
+        // When & Then
+        assertThatThrownBy(() -> searchService.searchProducts(searchTerm))
+                .isInstanceOf(ProductNotFoundException.class)
+                .hasMessageContaining("상품을 찾을 수 없습니다.");
+    }
+
+    @Test
+    public void autoCompleteNames_ValidSearchTerm_ShouldReturnNameList() {
+        // Given
+        String searchTerm = "Test";
+
+        // When
+        List<String> result = searchService.autoCompleteNames(searchTerm);
+
+        // Then
+        assertThat(result).isNotEmpty();
+        assertThat(result).contains(testProduct.getProductName(), testProduct.getCategoryName());
+    }
+
+    @Test
+    public void autoCompleteNames_InvalidSearchTerm_ShouldThrowException() {
+        // Given
+        String searchTerm = "";
+
+        // When & Then
+        assertThatThrownBy(() -> searchService.autoCompleteNames(searchTerm))
+                .isInstanceOf(InvalidSearchTermException.class)
+                .hasMessageContaining("검색어를 입력해주세요.");
+    }
+
+    @Test
+    public void getProductById_ValidId_ShouldReturnProduct() {
+        // Given
+        Long productId = testProduct.getProductId();
+
+        // When
         ProductCardListResponseDTO result = searchService.getProductById(productId);
 
-        // then
-        assertNotNull(result);
-        assertEquals(productId, result.getProductId());
-    }
-
-    // 예외 처리 테스트
-    @Test
-    public void searchProducts_InvalidSearchTerm_ThrowsException() {
-        assertThrows(InvalidSearchTermException.class, () -> {
-            searchService.searchProducts(null);
-        });
-        assertThrows(InvalidSearchTermException.class, () -> {
-            searchService.searchProducts("");
-        });
+        // Then
+        assertThat(result).isNotNull();
+        assertThat(result.getProductName()).isEqualTo(testProduct.getProductName());
     }
 
     @Test
-    public void autoCompleteNames_TooShortName_ThrowsException() {
-        assertThrows(InvalidSearchTermException.class, () -> {
-            searchService.autoCompleteNames("훈");
-        });
-    }
+    public void getProductById_InvalidId_ShouldThrowException() {
+        // Given
+        Long productId = -1L;
 
-    // 통합 테스트
-    @Test
-    public void searchService_IntegrationTest() {
-        String searchTerm = "통밀식빵";
-        List<ProductCardListResponseDTO> results = searchService.searchProducts(searchTerm);
-        assertFalse(results.isEmpty());
-        assertEquals(searchTerm, results.get(0).getProductName());
+        // When & Then
+        assertThatThrownBy(() -> searchService.getProductById(productId))
+                .isInstanceOf(ProductNotFoundException.class)
+                .hasMessageContaining("상품을 찾을 수 없습니다.");
     }
 }
+
+
